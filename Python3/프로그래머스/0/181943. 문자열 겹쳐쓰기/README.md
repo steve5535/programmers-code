8ddb7ca23422e765f720fcca013659c13829f0d0
SUNG SEON HYEOK <steve5535@naver.com>
2026-04-22 22:14:55 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 9.15 MB -BaekjoonHub

--- Python3/프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ Python3/프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943#) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 0.01 ms
+메모리: 9.15 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 21일 23:28:03
+2026년 04월 22일 22:14:53
 
 ### 문제 설명
 
--- Python3/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ Python3/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,4 +1,9 @@
+# def solution(my_string, overwrite_string, s):
+#     answer = ''
+#     answer = my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
+#     return answer
 def solution(my_string, overwrite_string, s):
-    answer = ''
-    answer = my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
+    list_string = list(my_string)
+    list_string[s:s+len(overwrite_string)] = overwrite_string
+    answer = ''.join(list_string)
     return answer
